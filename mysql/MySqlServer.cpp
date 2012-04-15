5e905c3c88180e0cf144065be0639e860efc4397
Michi Mutsuzaki <michi@cs.stanford.edu>
2012-04-15 02:39:01 -0700

    clean up mysql

--- mysql/MySqlServer.cpp
+++ mysql/MySqlServer.cpp
@@ -15,13 +15,11 @@
  */
 
 /**
- * This is a implementation of the mapkeeper interface that uses 
- * mysql.
+ * This is a implementation of the mapkeeper interface that uses mysql.
  */
 #include <mysql.h>
 #include <mysqld_error.h>
 #include <arpa/inet.h>
-#include <cstdio>
 #include "MapKeeper.h"
 #include <boost/thread/tss.hpp>
 #include <boost/lexical_cast.hpp>
@@ -31,22 +29,21 @@
 #include <transport/TServerSocket.h>
 #include <transport/TBufferTransports.h>
 
-
-using namespace ::apache::thrift;
-using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
-using namespace ::apache::thrift::concurrency;
-
-using boost::shared_ptr;
-
 using namespace mapkeeper;
+using boost::shared_ptr;
 
 class MySqlServer: virtual public MapKeeperIf {
 public:
     MySqlServer(const std::string& host, uint32_t port) :
         host_(host),
-        port_(port) {
+        port_(port),
+        mysql_(new boost::thread_specific_ptr<MYSQL>(destroyMySql)) {
+    }
+
+    ~MySqlServer() {
+      delete mysql_;
     }
 
     ResponseCode::type ping() {
@@ -54,35 +51,32 @@ public:
     }
 
     ResponseCode::type addMap(const std::string& mapName) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         std::string query = "create table " + escapeString(mapName) + 
             "(record_key varbinary(512) primary key, record_value longblob not null) engine=innodb";
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_TABLE_EXISTS_ERROR) {
                 return ResponseCode::MapExists;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 return ResponseCode::Error;
             }
         }
         return ResponseCode::Success;
-
     }
 
     ResponseCode::type dropMap(const std::string& mapName) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         std::string query = "drop table " + escapeString(mapName);
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_BAD_TABLE_ERROR) {
                 return ResponseCode::MapNotFound;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 return ResponseCode::Error;
             }
         }
@@ -90,19 +84,18 @@ public:
     }
 
     void listMaps(StringListResponse& _return) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         _return.responseCode = ResponseCode::Success;
         _return.values.clear();
         std::string query = "show tables";
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
-            fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+            uint32_t error = mysql_errno(mysql_->get());
+            fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
             return;
         }
 
-        MYSQL_RES* res = mysql_store_result(mysql.get());
+        MYSQL_RES* res = mysql_store_result(mysql_->get());
         MYSQL_ROW row;
 
         while ((row = mysql_fetch_row(res))) {
@@ -116,8 +109,7 @@ public:
               const std::string& startKey, const bool startKeyIncluded, 
               const std::string& endKey, const bool endKeyIncluded,
               const int32_t maxRecords, const int32_t maxBytes) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         std::string query = "select record_key, record_value from " + 
             escapeString(mapName) + " where record_key " + 
             (startKeyIncluded ? ">=" : ">") + " '" + escapeString(startKey) + "'";
@@ -133,20 +125,20 @@ public:
             query += " limit " + boost::lexical_cast<std::string>(maxRecords);
         }
 
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_NO_SUCH_TABLE) {
                 _return.responseCode = mapkeeper::ResponseCode::MapNotFound;
                 return;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 _return.responseCode = mapkeeper::ResponseCode::Error;
                 return;
             }
         }
 
-        MYSQL_RES* res = mysql_store_result(mysql.get());
+        MYSQL_RES* res = mysql_store_result(mysql_->get());
         MYSQL_ROW row;
 
         int32_t numBytes = 0;
@@ -165,28 +157,26 @@ public:
         }
         mysql_free_result(res);
         _return.responseCode = mapkeeper::ResponseCode::ScanEnded;
-
     }
 
     void get(BinaryResponse& _return, const std::string& mapName, const std::string& key) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
 
         std::string query = "select record_value from " + escapeString(mapName) + 
             " where record_key = '" + escapeString(key) + "'";
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_NO_SUCH_TABLE) {
                 _return.responseCode = ResponseCode::MapNotFound;
                 return;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 _return.responseCode = ResponseCode::Error;
                 return;
             }
         }
-        MYSQL_RES* res = mysql_store_result(mysql.get());
+        MYSQL_RES* res = mysql_store_result(mysql_->get());
         uint32_t numRows = mysql_num_rows(res);
         if (numRows == 0) {
             mysql_free_result(res);
@@ -211,20 +201,19 @@ public:
     }
 
     ResponseCode::type insert(const std::string& mapName, const std::string& key, const std::string& value) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         std::string query = "insert " + escapeString(mapName) + " values('" + 
             escapeString(key) + "', '" + 
             escapeString(value) + "')";
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_NO_SUCH_TABLE) {
                 return ResponseCode::MapNotFound;
             } else if (error == ER_DUP_ENTRY) {
                 return ResponseCode::RecordExists;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 return ResponseCode::Error;
             }
         }
@@ -232,21 +221,20 @@ public:
     }
 
     ResponseCode::type update(const std::string& mapName, const std::string& key, const std::string& value) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         std::string query = "update " + escapeString(mapName) + " set record_value = '" + 
             escapeString(value) + "' where record_key = '" +  escapeString(key) + "'";
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_NO_SUCH_TABLE) {
                 return ResponseCode::MapNotFound;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 return ResponseCode::Error;
             }
         }
-        uint64_t numRows = mysql_affected_rows(mysql.get());
+        uint64_t numRows = mysql_affected_rows(mysql_->get());
         if (numRows == 0) {
             return ResponseCode::RecordNotFound;
         } else if (numRows != 1) {
@@ -257,21 +245,20 @@ public:
     }
 
     ResponseCode::type remove(const std::string& mapName, const std::string& key) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         std::string query = "delete from " + escapeString(mapName) + 
             " where record_key = '" +  escapeString(key) + "'";
-        int result = mysql_real_query(mysql.get(), query.c_str(), query.length());
+        int result = mysql_real_query(mysql_->get(), query.c_str(), query.length());
         if (result != 0) {
-            uint32_t error = mysql_errno(mysql.get());
+            uint32_t error = mysql_errno(mysql_->get());
             if (error == ER_NO_SUCH_TABLE) {
                 return ResponseCode::MapNotFound;
             } else {
-                fprintf(stderr, "%d %s\n", error, mysql_error(mysql.get()));
+                fprintf(stderr, "%d %s\n", error, mysql_error(mysql_->get()));
                 return ResponseCode::Error;
             }
         }
-        uint64_t numRows = mysql_affected_rows(mysql.get());
+        uint64_t numRows = mysql_affected_rows(mysql_->get());
         if (numRows == 0) {
             return ResponseCode::RecordNotFound;
         } else if (numRows != 1) {
@@ -283,28 +270,27 @@ public:
 
 private:
     std::string escapeString(const std::string& str) {
-        static boost::thread_specific_ptr<MYSQL> mysql(destroyMySql);
-        initMySql(mysql);
+        initMySql();
         // http://dev.mysql.com/doc/refman/4.1/en/mysql-real-escape-string.html
         char buffer[2 * str.length() + 1];
-        uint64_t length = mysql_real_escape_string(mysql.get(), buffer, str.c_str(), str.length());
+        uint64_t length = mysql_real_escape_string(mysql_->get(), buffer, str.c_str(), str.length());
         return std::string(buffer, length);
     }
 
-    void initMySql(boost::thread_specific_ptr<MYSQL>& mysql) { 
-        if (mysql.get()) {
+    void initMySql() {
+        if (mysql_->get()) {
             return;
         }
         MYSQL* mysqlPtr = (MYSQL*)malloc(sizeof(MYSQL));
-        mysql.reset(mysqlPtr);
-        assert(mysql.get() == mysql_init(mysql.get()));
+        mysql_->reset(mysqlPtr);
+        assert(mysql_->get() == mysql_init(mysql_->get()));
         
         // Automatically reconnect if the connection is lost.
         // http://dev.mysql.com/doc/refman/5.0/en/mysql-options.html
         my_bool reconnect = 1;
-        assert(0 == mysql_options(mysql.get(), MYSQL_OPT_RECONNECT, &reconnect));
+        assert(0 == mysql_options(mysql_->get(), MYSQL_OPT_RECONNECT, &reconnect));
 
-        assert(mysql.get() == mysql_real_connect(mysql.get(), 
+        assert(mysql_->get() == mysql_real_connect(mysql_->get(), 
             host_.c_str(),  // hostname
             "root",         // user 
             NULL,           // password 
@@ -314,18 +300,19 @@ private:
             0               // flags
         ));
         std::string query = "create database if not exists mapkeeper";
-        assert(0 == mysql_real_query(mysql.get(), query.c_str(), query.length()));
+        assert(0 == mysql_real_query(mysql_->get(), query.c_str(), query.length()));
         query = "use mapkeeper";
-        assert(0 == mysql_real_query(mysql.get(), query.c_str(), query.length()));
-
+        assert(0 == mysql_real_query(mysql_->get(), query.c_str(), query.length()));
     }
 
     static void destroyMySql(MYSQL* mysql) {
         mysql_close(mysql);
+        free(mysql);
     }
 
     std::string host_;
     uint32_t port_;
+    boost::thread_specific_ptr<MYSQL>* mysql_;
 };
 
 int main(int argc, char **argv) {
