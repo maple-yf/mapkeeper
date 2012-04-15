6069fe3166490d6e89928c4b4679c0b46c8d8753
Michi Mutsuzaki <michi@cs.stanford.edu>
2012-04-15 11:50:26 -0700

    clean up bdb

--- bdb/BdbServerHandler.cpp
+++ bdb/BdbServerHandler.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <arpa/inet.h>
 #include <sstream>
 #include <cerrno>
 #include <dirent.h>
